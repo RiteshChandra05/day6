2d55521d7015d858bb957a857ed573255503dbde
RiteshChandra05 <kesha.riteshchandragupta@gmail.com>
2016-12-29 23:49:29 -0600

    constructor

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constuctor
+{
+    class Program
+    {
+        string xyz;
+        string abc;
+
+        public Program(string XYZ, string ABC)
+        {
+            this.xyz = XYZ;
+            this.abc = ABC;
+            
+        }
+        public void print()
+        {
+            Console.WriteLine(this.xyz + " " + this.abc);
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program("ritesh", "chandra");
+            p1.print();
+
+
+        }
+    }
+}
